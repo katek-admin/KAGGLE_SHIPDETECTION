546850f02189ec0fca86894533c43d97f80c680c
katek-admin <ekaterinakaz@gmail.com>
2024-07-14 19:16:10 +0300

    code restructure

--- model_inference.py
+++ model_inference.py
@@ -1,8 +1,8 @@
 import tensorflow as tf
 import os
 
-import data, config, utils
-from model_training import model
+import data, utils
+from model_training import trained_model
 
 
 def show_predictions(model, dataset=None, num=1):
@@ -20,10 +20,10 @@ def show_predictions(model, dataset=None, num=1):
         for image, mask in dataset.take(num):
             pred_mask = model.predict(image)
             utils.display([image[0], mask[0], utils.create_mask(pred_mask)])
-   
-        
-
+          
 #dataset = data.load_test_data()
 dataset = data.batched_train_dataset
-show_predictions(model = model, dataset = dataset, num = 10)
+
+print("Showing predictions...")
+show_predictions(model = trained_model, dataset = dataset, num = 10)
 
--- model_training.py
+++ model_training.py
@@ -3,32 +3,40 @@ from keras import optimizers
 import os
 
 import config, data, utils
-from unet_model import model
 
-# Load the model from the saved file
-if os.path.exists(os.getcwd()+ config.model_file):
-    print('Saved model is taken')
-    model = tf.keras.models.load_model(os.getcwd()+ config.model_file, custom_objects={'combined_loss': utils.combined_loss, 'dice_coefficient': utils.dice_coefficient})
-else:
-    model.compile(optimizer=optimizers.Adam(learning_rate=config.ADAM_LEARNING_RATE, epsilon=1e-06), loss=[utils.combined_loss], metrics=[utils.dice_coefficient])
 
-    callbacks_list = [
-        tf.keras.callbacks.EarlyStopping(
-            monitor="val_loss",
-            patience=2,
-        ),
-        tf.keras.callbacks.ModelCheckpoint(
-            filepath=os.getcwd() + config.model_file,
-            monitor="val_loss",
-            save_best_only=True,
-        )
-    ]
+def train():
+    from unet_model import model
 
-    print("Model fit starting...")
-    history = model.fit(
-        data.batched_train_dataset, 
-        epochs=config.EPOCHS, 
-        validation_data = data.batched_val_dataset,
-        callbacks=[callbacks_list]
-        )
+    # Load the model from the saved file
+    if os.path.exists(os.getcwd()+ config.model_file):
+        print('Saved model is taken')
+        model = tf.keras.models.load_model(os.getcwd()+ config.model_file, custom_objects={'combined_loss': utils.combined_loss, 'dice_coefficient': utils.dice_coefficient})
+    else:
+       
+        model.compile(optimizer=optimizers.Adam(learning_rate=config.ADAM_LEARNING_RATE, epsilon=1e-06), loss=[utils.combined_loss], metrics=[utils.dice_coefficient])
+
+        callbacks_list = [
+            tf.keras.callbacks.EarlyStopping(
+                monitor="val_loss",
+                patience=2,
+            ),
+            tf.keras.callbacks.ModelCheckpoint(
+                filepath=os.getcwd() + config.model_file,
+                monitor="val_loss",
+                save_best_only=True,
+            )
+        ]
+
+        print("Model fit starting...")
+        history = model.fit(
+            data.batched_train_dataset, 
+            epochs=config.EPOCHS, 
+            validation_data = data.batched_val_dataset,
+            callbacks=[callbacks_list]
+            )
+    
+    return model
+
+trained_model = train()
 
